2107f23e084b66649aac7e54463ff3ea5e1e89a7
aman-akhtar <amanakhtar881@gmail.com>
2024-04-21 22:59:44 +0530

    info: footer

--- src/Pages/CalculateAll.jsx
+++ src/Pages/CalculateAll.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import SipCalculatator from "./SipCalculator/SipCalculatator";
 import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 const CalculateAll = () => {
   return (
     <div>
       <Navbar />
       <p></p>
       <SipCalculatator />
+      <p></p>
+      <Footer />
     </div>
   );
 };
--- src/Pages/SipCalculator/SipCalculatator.jsx
+++ src/Pages/SipCalculator/SipCalculatator.jsx
@@ -219,242 +219,262 @@ const SipCalculatator = () => {
   const threshold = 10000; // Threshold after which to display values in
 
   return (
-    <div className="mx-2">
-      <p className="text-blue-950 font-semibold text-xl my-2 ">
-        Sip <span className="text-green-700">Calculator</span>
-      </p>
-      <div className="flex gap-2 my-2">
-        {["daily", "weekly", "monthly", "quarterly", "yearly"].map(
-          (interval) => (
-            <button
-              key={interval}
-              onClick={() => setInvestmentInterval(interval)}
-              className={`px-2 text-sm font-semibold ${
-                interval === investmentInterval
-                  ? "bg-green-200 text-green-950"
-                  : "bg-slate-200 text-slate-900"
-              } rounded`}
-            >
-              {getButtonTextAndColor(interval).text}
-            </button>
-          )
-        )}
-      </div>
-      <section className="my-5 flex flex-col gap-4 p-2 border-[2px] border-solid border-gray-300 rounded">
-        <div className="h-[70px] flex flex-col justify-between">
-          <aside className="flex justify-between items-end relative">
-            <p className="capitalize">{investmentInterval} Investment</p>
+    <div className="lg:mx-5 mx-2">
+      <header className="lg:flex justify-between items-baseline text-2xl mt-3">
+        <p className="text-blue-950 font-semibold text-[30px]  ">
+          Sip <span className="text-green-700">Calculator</span>
+        </p>
+        <div className="flex gap-2 my-2 ">
+          {["daily", "weekly", "monthly", "quarterly", "yearly"].map(
+            (interval) => (
+              <button
+                key={interval}
+                onClick={() => setInvestmentInterval(interval)}
+                className={`px-2 lg:text-[15px] text-[12px] font-semibold ${
+                  interval === investmentInterval
+                    ? "bg-green-200 text-green-950"
+                    : "bg-slate-200 text-slate-900"
+                } rounded`}
+              >
+                {getButtonTextAndColor(interval).text}
+              </button>
+            )
+          )}
+        </div>
+      </header>
+      <main className="lg:flex justify-between w-full gap-3">
+        <section className="my-5 flex flex-col gap-4 p-2 border-[2px] border-solid border-gray-300 lg:w-1/2 rounded">
+          <div className="h-[70px] flex flex-col justify-between">
+            <aside className="flex justify-between items-end relative">
+              <p className="capitalize">{investmentInterval} Investment</p>
+              <input
+                type="text"
+                value={formatIndianNumber(monthlyInvestment)}
+                onChange={(e) =>
+                  handleInputChange(
+                    e,
+                    setMonthlyInvestment,
+                    setMonthlyInvestmentSlider
+                  )
+                }
+                className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px] w-[150px]"
+              />
+              <span className="absolute right-2 p-[2px] font-semibold italic text-green-700">
+                Rs
+              </span>
+            </aside>
+            <p className="text-right text-[10px] font-semibold text-red-500">
+              {monthlyInvestment <= 0 && <p>Investment Cant Be ZERO</p>}
+            </p>
             <input
-              type="text"
-              value={formatIndianNumber(monthlyInvestment)}
+              type="range"
+              min="100"
+              max={getMaxInvestmentValue()}
+              value={monthlyInvestmentSlider}
               onChange={(e) =>
-                handleInputChange(
+                handleSliderChange(
                   e,
                   setMonthlyInvestment,
                   setMonthlyInvestmentSlider
                 )
               }
-              className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px] w-[150px]"
+              className="bg-slate-100 text-right  rounded items-end w-full"
             />
-            <span className="absolute right-2 p-[2px] font-semibold italic text-green-700">
+          </div>
+          <div className="h-[70px] flex flex-col justify-between">
+            <aside className="flex justify-between items-end relative">
+              <p>Expected Return</p>
+              <input
+                type="text"
+                value={expectedReturn}
+                onChange={(e) =>
+                  handleInputChange(
+                    e,
+                    setExpectedReturn,
+                    setExpectedReturnSlider
+                  )
+                }
+                className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px] w-[150px]"
+              />
+              <span className="absolute right-2 p-[2px] font-semibold italic text-green-700">
+                %
+              </span>
+            </aside>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value={expectedReturnSlider}
+              onChange={(e) =>
+                handleSliderChange(
+                  e,
+                  setExpectedReturn,
+                  setExpectedReturnSlider
+                )
+              }
+              className="bg-slate-100 text-right  rounded items-end w-full"
+            />
+          </div>
+          <div className="h-[70px] flex flex-col justify-between">
+            <aside className="flex justify-between items-end relative">
+              <p>Time Period</p>
+              <input
+                type="text"
+                value={timePeriod}
+                onChange={(e) =>
+                  handleInputChange(e, setTimePeriod, setTimePeriodSlider)
+                }
+                className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px] w-[150px]"
+              />
+              <span className="absolute right-2 p-[2px] font-semibold italic text-green-700">
+                Yr
+              </span>
+            </aside>
+            <input
+              type="range"
+              min="1"
+              max="100"
+              value={timePeriodSlider}
+              onChange={(e) =>
+                handleSliderChange(e, setTimePeriod, setTimePeriodSlider)
+              }
+              className="bg-slate-100 text-right  rounded items-end w-full"
+            />
+          </div>
+        </section>
+        <section className="my-5 flex flex-col  p-2 border-[2px] border-solid border-gray-300 lg:w-1/2 rounded">
+          <div className="w-full">
+            <p>Invested amount &nbsp;</p>
+            <input
+              type="text"
+              value={investedAmount?.toLocaleString("en-IN", {
+                maximumFractionDigits: 0,
+              })}
+              className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px]     w-full "
+            />{" "}
+            <span className="absolute lg:right-[35px] right-[26px] p-[2px] font-semibold italic text-green-700">
               Rs
             </span>
-          </aside>
-          <p className="text-right text-[10px] font-semibold text-red-500">
-            {monthlyInvestment <= 0 && <p>Investment Cant Be ZERO</p>}
-          </p>
-          <input
-            type="range"
-            min="100"
-            max={getMaxInvestmentValue()}
-            value={monthlyInvestmentSlider}
-            onChange={(e) =>
-              handleSliderChange(
-                e,
-                setMonthlyInvestment,
-                setMonthlyInvestmentSlider
-              )
-            }
-            className="bg-slate-100 text-right  rounded items-end w-full"
-          />
-        </div>
-        <div className="h-[70px] flex flex-col justify-between">
-          <aside className="flex justify-between items-end relative">
-            <p>Expected Return</p>
+          </div>
+          <div className="w-full">
+            <p>Est returns &nbsp;</p>
             <input
               type="text"
-              value={expectedReturn}
-              onChange={(e) =>
-                handleInputChange(e, setExpectedReturn, setExpectedReturnSlider)
-              }
-              className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px] w-[150px]"
-            />
-            <span className="absolute right-2 p-[2px] font-semibold italic text-green-700">
-              %
-            </span>
-          </aside>
-          <input
-            type="range"
-            min="1"
-            max="100"
-            value={expectedReturnSlider}
-            onChange={(e) =>
-              handleSliderChange(e, setExpectedReturn, setExpectedReturnSlider)
-            }
-            className="bg-slate-100 text-right  rounded items-end w-full"
-          />
-        </div>
-        <div className="h-[70px] flex flex-col justify-between">
-          <aside className="flex justify-between items-end relative">
-            <p>Time Period</p>
-            <input
-              type="text"
-              value={timePeriod}
-              onChange={(e) =>
-                handleInputChange(e, setTimePeriod, setTimePeriodSlider)
-              }
-              className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px] w-[150px]"
-            />
-            <span className="absolute right-2 p-[2px] font-semibold italic text-green-700">
-              Yr
-            </span>
-          </aside>
-          <input
-            type="range"
-            min="1"
-            max="100"
-            value={timePeriodSlider}
-            onChange={(e) =>
-              handleSliderChange(e, setTimePeriod, setTimePeriodSlider)
-            }
-            className="bg-slate-100 text-right  rounded items-end w-full"
-          />
-        </div>
-      </section>
-      <section className="my-5 flex flex-col  p-2 border-[2px] border-solid border-gray-300 rounded">
-        <div className="w-full">
-          <p>Invested amount &nbsp;</p>
-          <input
-            type="text"
-            value={investedAmount?.toLocaleString("en-IN", {
-              maximumFractionDigits: 0,
-            })}
-            className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px]     w-full "
-          />{" "}
-          <span className="absolute right-6 p-[2px] font-semibold italic text-green-700">
-            Rs
-          </span>
-        </div>
-        <div className="w-full">
-          <p>Est returns &nbsp;</p>
-          <input
-            type="text"
-            value={returnOnInvestment?.toLocaleString("en-IN", {
-              maximumFractionDigits: 0,
-            })}
-            className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px]     w-full"
-          />{" "}
-          <span className="absolute right-6 p-[2px] font-semibold italic text-green-700">
-            Rs
-          </span>
-        </div>
-        <div className="w-full">
-          {" "}
-          <p>Total Maturity &nbsp;</p>
-          <input
-            type="text"
-            value={(returnOnInvestment + investedAmount)?.toLocaleString(
-              "en-IN",
-              {
+              value={returnOnInvestment?.toLocaleString("en-IN", {
                 maximumFractionDigits: 0,
-              }
-            )}
-            className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px]      w-full"
-          />{" "}
-          <span className="absolute right-6 p-[2px] font-semibold italic text-green-700">
-            Rs
-          </span>
-        </div>
-      </section>
-      <div className="w-full">
-        <ReactApexChart
-          options={{
-            labels: ["Invested ", "Return"], // Labels for the pie chart
-            title: {
-              text: "PAI",
-            },
-            dataLabels: {
-              enabled: true, // Enable data labels for the pie chart
-            },
-            tooltip: {
-              enabled: true,
-              y: {
-                formatter: function (val) {
-                  return "Amount: " + val; // Customize tooltip format
+              })}
+              className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px]     w-full"
+            />{" "}
+            <span className="absolute lg:right-[35px] right-[26px] p-[2px] font-semibold italic text-green-700">
+              Rs
+            </span>
+          </div>
+          <div className="w-full">
+            {" "}
+            <p>Total Maturity &nbsp;</p>
+            <input
+              type="text"
+              value={(returnOnInvestment + investedAmount)?.toLocaleString(
+                "en-IN",
+                {
+                  maximumFractionDigits: 0,
+                }
+              )}
+              className="bg-slate-100 text-right px-1 pr-8 rounded items-end h-[30px]      w-full"
+            />{" "}
+            <span className="absolute lg:right-[35px] right-[26px] p-[2px] font-semibold italic text-green-700">
+              Rs
+            </span>
+          </div>
+        </section>
+      </main>
+      <div className="w-full lg:flex gap-3 justify-between">
+        <aside className="lg:w-1/2 ">
+          <ReactApexChart
+            options={{
+              labels: ["Invested ", "Return"], // Labels for the pie chart
+              title: {
+                text: "PAI",
+              },
+              dataLabels: {
+                enabled: true, // Enable data labels for the pie chart
+              },
+              tooltip: {
+                enabled: true,
+                y: {
+                  formatter: function (val) {
+                    return "Amount: " + val; // Customize tooltip format
+                  },
                 },
               },
-            },
-          }}
-          series={[investedAmount, returnOnInvestment]} // Assign your calculated values here
-          type="pie" // Set chart type to "pie"
-          height={300}
-        />{" "}
-      </div>
-      <ReactApexChart
-        options={{
-          chart: {
-            id: "bar-chart",
-          },
-          title: {
-            text: "BAR",
-          },
-          plotOptions: {
-            bar: {
-              horizontal: false,
-              columnWidth: "55%",
-              endingShape: "rounded",
-            },
-          },
-          dataLabels: {
-            enabled: false,
-          },
-          stroke: {
-            show: true,
-            width: 2,
-            colors: ["transparent"],
-          },
-          xaxis: {
-            categories: ["Investment vs Return"], // X-axis categories
-          },
-          yaxis: {
-            title: {
-              text: "Amount",
-            },
-            labels: {
-              formatter: function (value) {
-                return value.toFixed(2);
+            }}
+            series={[investedAmount, returnOnInvestment]} // Assign your calculated values here
+            type="pie" // Set chart type to "pie"
+            height={300}
+          />{" "}
+        </aside>
+        <aside className="lg:w-1/2 ">
+          <ReactApexChart
+            options={{
+              chart: {
+                id: "bar-chart",
               },
-            },
-          },
-          dataLabels: {
-            enabled: false,
-          },
-        }}
-        series={[
-          {
-            name: "Invested Amount",
-            data: [investedAmount],
-          },
-          {
-            name: "Return On Investment",
-            data: [returnOnInvestment],
-          },
-        ]}
-        type="bar"
-        height={300}
-      />
+              title: {
+                text: "BAR",
+              },
+              plotOptions: {
+                bar: {
+                  horizontal: false,
+                  columnWidth: "55%",
+                  endingShape: "rounded",
+                },
+              },
+              dataLabels: {
+                enabled: false,
+              },
+              stroke: {
+                show: true,
+                width: 2,
+                colors: ["transparent"],
+              },
+              xaxis: {
+                categories: ["Investment vs Return"], // X-axis categories
+              },
+              yaxis: {
+                title: {
+                  text: "Amount",
+                },
+                labels: {
+                  formatter: function (value) {
+                    return value.toFixed(2);
+                  },
+                },
+              },
+              dataLabels: {
+                enabled: false,
+              },
+            }}
+            series={[
+              {
+                name: "Invested Amount",
+                data: [investedAmount],
+              },
+              {
+                name: "Return On Investment",
+                data: [returnOnInvestment],
+              },
+            ]}
+            type="bar"
+            height={300}
+          />{" "}
+        </aside>
+      </div>
       <aside className="overflow-auto">
-        <div className={`${timePeriod > 20 ? "w-[1000px]" : "w-full"}`}>
+        <div
+          className={`${
+            timePeriod > 20 ? "w-[1000px] lg:w-[98%]" : "lg:w-[98%]"
+          }`}
+        >
           <ReactApexChart
             options={{
               chart: {
@@ -525,8 +545,12 @@ const SipCalculatator = () => {
           />
         </div>
       </aside>{" "}
-      <aside className="overflow-auto">
-        <div className={`${timePeriod > 20 ? "w-[1000px]" : "w-full"}`}>
+      <aside className="overflow-auto lg:overflow-hidden">
+        <div
+          className={`${
+            timePeriod > 20 ? "w-[1000px] lg:w-[98%]" : "lg:w-[98%]"
+          }`}
+        >
           <ReactApexChart
             options={{
               chart: {
